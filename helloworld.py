ad1cf870279a090716f5404110aaf87647c13a71
Mischa <mhgrunberg@outlook.com>
2021-03-18 19:18:18 -0400

    Add hello world
    Woop

---
+++ helloworld.py
@@ -0,0 +1,15 @@
+# create name and country
+name = 'Mischa'
+country = 'Germany'
+
+age = 500
+hourly_wage = 20
+
+satisfied = True
+
+daily_wage = hourly_wage * 8
+
+print("You live in " + country)
+print("You are " + str(age) + " years old")
+print("You make", str(daily_wage), "per day")
+print("Are you satisfied with you current wage? " + str(satisfied))
